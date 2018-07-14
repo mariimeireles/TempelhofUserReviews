8bcacf4ecb6f9a9d0bae5ecfdcd10677ba9fec78
mariimeireles <marianaribeiromeireles@gmail.com>
2018-07-14 08:50:33 +0200

    new review view

--- TempelhofUserReviews/Presentation/NewReview/View/NewReviewViewController.swift
+++ TempelhofUserReviews/Presentation/NewReview/View/NewReviewViewController.swift
@@ -2,13 +2,21 @@
 
 import UIKit
 
-final class NewReviewViewController: UIViewController {
+final class NewReviewViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setTextView()
     }
     
-
+    private func setTextView() {
+        textView.layer.borderWidth = 1
+        let borderColor = UIColor(red:0.89, green:0.89, blue:0.89, alpha:1.0)
+        textView.layer.borderColor = borderColor.cgColor
+        textView.clipsToBounds = true
+        textView.layer.cornerRadius = 5
+    }
     
-
 }
